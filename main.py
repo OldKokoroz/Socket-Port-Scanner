030c8ced146a4a696b13a7c4a09872d71cd6f5a5
Deus <119631661+OldKokoroz@users.noreply.github.com>
2024-01-01 22:55:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ while True:
     Range of ports you want to scan : <int>-<int>\n
     Enter port range : """)
 
-    port_range_valid = port_range_pattern.search(port_range.replace(" ", ""))
+    port_range_valid = port_range_pattern.search(port_range.replace(" ", " "))
     if port_range_valid:
         port_min = int(port_range_valid.group(1))
         port_max = int(port_range_valid.group(2))
@@ -28,7 +28,7 @@ while True:
 for port in range(port_min, port_max + 1):
     try:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.settimeout(0.5)
+            s.settimeout(1)
             s.connect((ip_add_entered, port))
             open_ports.append(port)
     except:
