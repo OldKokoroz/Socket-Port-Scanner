bb3dda0148e5e51fa9dd3a0cbb513e19e5b109ba
Deus <119631661+OldKokoroz@users.noreply.github.com>
2024-02-19 18:21:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ while True:
     Range of ports you want to scan : 0-1000\n
     Enter port range : """)
 
-    port_range_valid = port_range_pattern.search(port_range.replace(" ", " "))
+    port_range_valid = port_range_pattern.search(port_range.replace(" ", ""))
     if port_range_valid:
         port_min = int(port_range_valid.group(1))
         port_max = int(port_range_valid.group(2))
