4740416cd618c6d1d6fa93d26e5add3aedc0be27
Deus <119631661+OldKokoroz@users.noreply.github.com>
2024-02-26 23:04:50 +0300

    Add files via upload

---
+++ database.py
@@ -0,0 +1,8 @@
+# Ports and protocols
+
+ports_dict = {"21": "FTP",
+              "22": "SSH",
+              "23": "TELNET",
+              "25": "SMTP",
+              "80": "HTTP"
+              }
